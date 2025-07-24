ff9e1707064fd178f9fcf9b1a4d20ea766c09bfd
anusha26715 <prathikantamanu@gmail.com>
2025-07-24 23:38:04 +0530

    Pattern Practice Completed

--- num-pattern-assignment-4/pattern-practice.js
+++ num-pattern-assignment-4/pattern-practice.js
@@ -114,3 +114,199 @@ let input5 = 5
 input5Pattern(input5)
 
 // end time:- 2:34 am (24 july 2025)
+
+// start time:- 12:19 pm (24 july 2025)
+
+// 6) Descending Triangle from N
+// Input: 5
+// Output: 5 4 3 2 1
+//           5 4 3 2
+//             5 4 3
+//               5 4
+//                 5
+
+const input6Pattern = (n)=> {
+  for (let i=0;i<n;i++){
+    let arr = []
+    let space = "  "
+    for (let j=n;j>(0+i);j--){
+      arr.push(j)
+    }
+    console.log(space.repeat(i)+arr.join(" "))
+  }
+}
+
+let input6 = 5
+input6Pattern(input6)
+
+
+// 7) Triangle pattern from N
+// Input: 5
+// Output: 5 4 3 2 1
+//           4 3 2 1
+//             3 2 1
+//               2 1
+//                 1
+
+const input7Pattern = (n)=> {
+  for (let i=0;i<n;i++){
+    let arr = []
+    let space = "  "
+    for (let j=n-i;j>0;j--){
+      arr.push(j)
+    }
+    console.log(space.repeat(i)+arr.join(" "))
+  }
+}
+
+let input7 = 5
+input7Pattern(input7)
+
+// 8) Triangle Pattern from N
+// Input: 5
+// Output: 1 2 3 4 5
+//           1 2 3 4
+//             1 2 3
+//               1 2
+//                 1
+
+const input8Pattern = (n)=> {
+  for (let i=0;i<n;i++){
+    let arr = []
+    let space = "  "
+    for (let j=n-i;j>0;j--){
+      arr.push(j)
+    }
+    console.log(space.repeat(i)+arr.reverse().join(" "))
+  }
+}
+
+let input8 = 5
+input8Pattern(input8)
+
+// 9) Triangle Pattern from N
+// Input: 5
+// Output: 1 2 3 4 5
+//           2 3 4 5
+//             3 4 5
+//               4 5
+//                 5
+
+const input9Pattern = (n)=> {
+  for (let i=0;i<n;i++){
+    let arr = []
+    let space = "  "
+    for (let j=i+1;j<=n;j++){
+      arr.push(j)
+    }
+    console.log(space.repeat(i)+arr.join(" "))
+  }
+}
+
+let input9 = 5
+input9Pattern(input9)
+
+
+// 10) Triangle Pattern from N
+// Input: 5
+// Output: A B C D E
+//           A B C D
+//             A B C
+//               A B
+//                 A
+
+function input10Pattern(n) {
+  for (let i = 0; i < n; i++) {
+    let rowStr = [];
+    let space = "  "
+    for (let j=0;j<n-i;j++){
+      rowStr.push(String.fromCharCode(65+j))
+    }
+    console.log(space.repeat(i)+rowStr.join(" "));
+  }
+}
+
+let input10 = 5
+input10Pattern(input10)
+
+// 11) Triangle Pattern from N
+// Input: 5
+// Output: A B C D E
+//           B C D E
+//             C D E
+//               D E
+//                 E
+
+function input11Pattern(n) {
+  for (let i = 0; i < n; i++) {
+    let rowStr = [];
+    let space = "  "
+    for (let j=0;j<n-i;j++){
+      rowStr.push(String.fromCharCode(65+i+j))
+    }
+    console.log(space.repeat(i)+rowStr.join(" "));
+  }
+}
+
+let input11 = 5
+input11Pattern(input11)
+
+
+// 12) Triangle Pattern from N
+
+function input12Pattern(n) {      // Input: 5
+  let space = "  "                // Output:        1 
+  for (let i=0;i<n;i++){          //              2 1 2
+    let subArray1 = []            //            3 2 1 2 3
+    for(let j=1;j<=i+1;j++){      //          4 3 2 1 2 3 4
+      subArray1.push(j)           //        5 4 3 2 1 2 3 4 5
+    }
+    let subArray2 = []
+    for(let k=1;k<i+1;k++) subArray2.push(k+1);
+    let nestedArray = [...subArray1.reverse(),...subArray2]
+    console.log(space.repeat(n-i)+nestedArray.join(" "))
+  }
+}
+
+let input12 = 5
+input12Pattern(input12)
+
+// 13) Triangle Pattern from N
+
+function input13Pattern(n) {      // Input: 5
+  let space = "  "                // Output:        1 
+  for (let i=0;i<n;i++){          //              1 2 1
+    let subArray1 = []            //            1 2 3 2 1
+    for(let j=1;j<=i+1;j++){      //          1 2 3 4 3 2 1
+      subArray1.push(j)           //        1 2 3 4 5 4 3 2 1
+    }
+    let subArray2 = []
+    for(let k=1;k<i+1;k++) subArray2.push(k);
+    let nestedArray = [...subArray1,...subArray2.reverse()]
+    console.log(space.repeat(n-i)+nestedArray.join(" "))
+  }
+}
+
+let input13 = 5
+input13Pattern(input13)
+
+// 14) Triangle Pattern from N
+
+function input14Pattern(n) {      // Input: 5
+  let space = "  "                // Output:        5 
+  for (let i=0;i<n;i++){          //              4 5 4
+    let subArray1 = []            //            3 4 5 4 3 
+    for(let j=1;j<=i+1;j++){      //          2 3 4 5 4 3 2 
+      subArray1.push(n-j+1)       //        1 2 3 4 5 4 3 2 1
+    }
+    let subArray2 = []
+    for(let k=1;k<i+1;k++) subArray2.push(n-k);
+    let nestedArray = [...subArray1.reverse(),...subArray2]
+    console.log(space.repeat(n-i)+nestedArray.join(" "))
+  }
+}
+
+let input14 = 5
+input14Pattern(input14)
+
+// end time- 11:36 pm(24 july 2025)
